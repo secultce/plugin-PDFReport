a07b0ef0afebd116f3f204a22931679b4e56901d
Junior-Shyko <franciscoanto@gmail.com>
2021-11-16 19:32:37 -0300

    Criado entidade Pdf e alguns metodos

--- Entities/Pdf.php
+++ Entities/Pdf.php
@@ -13,5 +13,26 @@ class Pdf extends \MapasCulturais\Entity{
         return $app->repo('RegistrationMeta')->findBy(['key' => $body]);
               
     }
+
+    static public function getNameField($id) {
+        $app = App::i();
+        $body = 'field_'.$id;
+        return $app->repo('RegistrationFieldConfiguration')->findBy(['owner' => $id]);
+    }
+
+    static public function mask($val, $mask) {
+        $maskared = '';
+        $k = 0;
+        for($i = 0; $i<=strlen($mask)-1; $i++) {
+            if($mask[$i] == '#') {
+                if(isset($val[$k])) $maskared .= $val[$k++];
+            } else {
+                if(isset($mask[$i])) $maskared .= $mask[$i];
+            }
+        }
+        return $maskared;
+    }
+
+    //static public function getAgente
 }
 
