d437f9dfea54ed56187a55edc21c2b65c92438a7
Shyko-Junior <franciscoanto@gmail.com>
2021-11-22 16:46:46 -0300

    Impressão de inscritos

--- views/pdf/header.php
+++ views/pdf/header.php
@@ -1,15 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link type="text/css" href="bootstrap.min.css" rel="stylesheet" />
+    <link type="text/css" href="stylePdfReport.css" rel="stylesheet" />
+    <link type="text/css" href="<?php $this->asset('css/bootstrap.min.css') ?>" rel="stylesheet" />
+    <link type="text/css" href="<?php $this->asset('css/stylePdfReport.css') ?>" rel="stylesheet" />
+    <script src="<?php $this->asset('js/pdfreport/jquery.min.js') ?>"></script>
+    <style>
+            @page {  margin: 24px 24px;  }
+            footer { position: fixed; bottom: -15px; left: 0px; right: 0px;height: 50px; border-top: 1px solid #c3c3c3;
+             color: rgba(0, 0, 0, 0.6);}
+            
+    </style>
+</head>
+<body>
+<div class="container">
+    <br>
+    <a href="#" class="btn btn-primary" id="btn-print-report" >
+        <i class="fa fa-print"></i>
+        Imprimir Relatório
+    </a>
+</div>
 <table width="100%" style="height: 100px;">
     <thead>
-        <tr>
-            <td  width="50%">                   
-                <img src="<?php echo PLUGINS_PATH.'PDFReport/assets/img/logo-saude.png'; ?>" style="float:left;"/>
-                <!-- <img src="<?php $this->asset('img/logo-saude.png') ?>"  class="pull-left" > -->
-
+        <tr class="">
+            <td>                   
+                <img src="<?php $this->asset('img/logo-saude.png') ?>"  class="pull-left" >
             </td>
-            <td width="50%" style="float:right;">
-            <!-- <img src="<?php $this->asset('img/ESP-CE-ORGAO-SEC-INVERTIDA-WEB2_3.png') ?>" class="pull-right" alt=""> -->
-                <img src="<?php echo PLUGINS_PATH.'PDFReport/assets/img/ESP-CE-ORGAO-SEC-INVERTIDA-WEB2_3.png'; ?>" 
-                 style="margin-left: 100px"/>
+            <td>
+            <img src="<?php $this->asset('img/ESP-CE-ORGAO-SEC-INVERTIDA-WEB2_3.png') ?>" class="pull-right" alt="">
             </td>
         </tr>
     </thead>
--- views/pdf/subscribers.php
+++ views/pdf/subscribers.php
@@ -1,35 +1,46 @@
 <?php 
-    $this->layout = 'nolayout'; 
+    $this->layout = 'nolayout-pdf'; 
     $sub = $app->view->jsObject['subscribers'];
     $nameOpportunity = $sub[0]->opportunity->name;
     //Objeto Oportunidade
     $op = $app->view->jsObject['opp'];
-    include_once('style.php');  
-    // dump($sub);
-    // die;
+    include_once('style.php');
+    //VERIFICANDO SE A OPORTUNIDADE TEM CATEGORIA  
+    $isCategory = false;
+    if(is_array($op->registrationCategories) && count($op->registrationCategories) > 0) {
+        $isCategory = true;
+    }
+
 ?>
 </style>
 <div class="container">
-    <?php include_once('header.php'); ?>
-    
+    <?php include_once('header.php'); 
+    if($op->avatar) :
+    ?>
+    <table  style="height: 100px; margin-bottom:24px; margin-top:24px;">
+        <thead>
+            <tr>
+                <td>
+                    <img src="<?php echo $op->getFile('header')->url; ?>" alt="" srcset="">                
+                </td>
+            </tr>
+        </thead>
+    </table>
+    <?php endif; ?>    
 
-    <table width="100%" style="height: 100px; margin-bottom:40px;">
+    <table width="100%" style="height: 100px; margin-bottom:40px; margin-top:40px;">
         <thead>
             <tr class="">
                 <td style="width: 10%;">                   
-
-                    <!-- <img src="<?php $this->asset('img/logo-saude.png') ?>"  class="pull-left" > -->
                     <?php if(!empty($op->files['avatar'])): ?>
-                        <img src="<?php echo $op->files['avatar']->path; ?>"  style="width: 80px; height: 80px;">
+                        <img src="<?php echo $op->avatar->transform('avatarSmall')->url; ?>"  style="width: 80px; height: 80px;">
                     <?php else: ?>
-                        <img src="<?php echo THEMES_PATH.'BaseV1/assets/img/avatar--opportunity.png'; ?>" style="width: 80px; height: 80px;">
-                    <!-- <label for=""><?php echo THEMES_PATH.'BaseV1/assets/img/avatar--opportunity.png'; ?></label> -->
+                        <img src="<?php $this->asset('img/pdfreport/avatar--opportunity.png') ?>" style="width: 80px; height: 80px;">
                     <?php endif; ?>
-
                 </td>
                 <td style="width: 90%;">
-                <!-- <img src="<?php $this->asset('img/ESP-CE-ORGAO-SEC-INVERTIDA-WEB2_3.png') ?>" class="pull-right" alt=""> -->
-                    <label for="" class="title-edital">Edital</label><br>
+                
+                    <label class="title-edital">Edital</label><br>
                     <label class="sub-title-edital"><?php echo $op->ownerEntity->name; ?></label>
                     <br>
                     <label for="" class="title-edital">Oportunidade</label><br>
@@ -39,17 +50,6 @@
         </thead>
     </table>
 
-    <div class="row">
-        <div class="container">
-            <div class="col-md-12" class="div-categoria" style="background: #1F4E37; border-radius: 8px;color: #FFFFFF;
-padding: 4px 8px;">
-                <label for="">Categoiria 01</label>
-            </div>
-        </div>
-        <div class="col-md-12">
-            <br>
-        </div>
-    </div>
 
     <div class="row">
         <div class="container">
@@ -59,7 +59,9 @@ padding: 4px 8px;">
             <tr style="color: #2D3540;">
                 <th>Inscrição</th>
                 <th>Agente</th>
-                <th>Aval. Preliminar</th>
+                <?php if($isCategory) : ?>
+                <th>Categoria</th>
+                <?php endif; ?>
                 <th>Status</th>
             </tr>
         </thead>
@@ -69,7 +71,9 @@ padding: 4px 8px;">
             <tr>
                 <td><?php echo $value->number; ?></td>
                 <td><?php echo $agent->name; ?></td>
-                <td><?php echo $value->preliminaryResult; ?></td>
+                <?php if($isCategory) : ?>
+                <td><?php echo $value->category !== "" ? $value->category : "Não Informado"; ?></td>
+                <?php endif; ?>
                 <td><?php
                     $status = '';
                         switch ($value->status) {
@@ -103,4 +107,4 @@ padding: 4px 8px;">
 
 
 </div>
-<?php //include_once('footer.php'); ?>
+<?php include_once('footer.php'); ?>
