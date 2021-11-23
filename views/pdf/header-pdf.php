7303b71497337260db3798d785bdc8a70ddbe569
Shyko-Junior <franciscoanto@gmail.com>
2021-11-23 11:42:07 -0300

    Add header somente para pdf

---
+++ views/pdf/header-pdf.php
@@ -0,0 +1,66 @@
+<?php 
+    $this->layout = 'nolayout-pdf'; 
+    $reg = $app->view->regObject['ins'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link type="text/css" href="bootstrap.min.css" rel="stylesheet" />
+    <link type="text/css" href="stylePdfReport.css" rel="stylesheet" />
+    <link type="text/css" href="<?php $this->asset('css/bootstrap.min.css') ?>" rel="stylesheet" />
+    <link type="text/css" href="<?php $this->asset('css/stylePdfReport.css') ?>" rel="stylesheet" />
+    <script src="<?php $this->asset('js/pdfreport/jquery.min.js') ?>"></script>
+
+</head>
+<body>
+<?php if(isset($_GET['idopportunityReport']) && $_GET['idopportunityReport'] > 0) : ?>
+<div class="container">
+    <br>
+    <a href="#" class="btn btn-primary" id="btn-print-report" >
+        <i class="fa fa-print"></i>
+        Imprimir Relatório
+    </a>
+</div>
+<?php endif; ?>
+<table width="100%" style="height: 100px;">
+    <thead>
+        <tr class="">
+            <td>                   
+                <img src="<?php echo PLUGINS_PATH.'PDFReport/assets/img/logo-saude.png'; ?>" style="float:left;"/>
+            </td>
+            <td>
+            <img src="<?php echo PLUGINS_PATH.'PDFReport/assets/img/ESP-CE-ORGAO-SEC-INVERTIDA-WEB2_3.png'; ?>" style="margin-left: 300px;" alt="">
+            </td>
+        </tr>
+    </thead>
+</table>
+<div class="content-info-edital">
+<table width="100%" style="height: 100px; margin-bottom:40px; margin-top:40px;">
+        <thead>
+            <tr class="">
+                <td style="width: 10%;">                   
+                    <?php if(!empty( $reg->opportunity->files['avatar'])): ?>
+                        <img src="<?php echo $reg->opportunity->avatar->transform('avatarSmall')->path; ?>"  style="width: 80px; height: 80px;">
+                    <?php else: ?>
+                        <img src="<?php echo PLUGINS_PATH.'PDFReport/assets/img/pdfreport/avatar--opportunity.png'; ?>" style="width: 80px; height: 80px;">
+                    <?php endif; ?>
+                </td>
+                <td style="width: 90%;">
+                
+                   
+                   <label class="title-edital">Edital</label><br>
+                    <label class="sub-title-edital"><?php echo $reg->opportunity->ownerEntity->name; ?></label>
+                    <br>
+                    <label for="" class="title-edital">Oportunidade</label><br>
+                    <label class="sub-title-edital"><?php echo $reg->opportunity->name; ?></label>
+                   
+                </td>
+            </tr>
+        </thead>
+    </table>
+                    </div>
+
