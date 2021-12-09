fbb6a2a7850a92c77ce2a6749165580287cd14e1
Pedro <lspedrovitor@gmail.com>
2021-12-09 11:02:07 -0300

    feat: mostrando as secoes com nota

--- views/pdf/technical-category.php
+++ views/pdf/technical-category.php
@@ -14,6 +14,35 @@
         return ($item1->consolidatedResult < $item2->consolidatedResult) ? 1 : -1;
     }
     usort($sub,'invenDescSort');
+
+    function getSectionNote($opp, $registration, $section_id){
+        $total = 0.00;
+        $app = App::i();
+        $committee = $opp->getEvaluationCommittee();
+        $users = [];
+        foreach ($committee as $item) {
+            $users[] = $item->agent->user->id;
+        }
+        $evaluations = $app->repo('RegistrationEvaluation')->findByRegistrationAndUsersAndStatus($registration, $users);
+        foreach ($evaluations as $eval){
+            $cfg = $eval->getEvaluationMethodConfiguration();
+            $category = $eval->registration->category;
+            $totalSection = 0.00;
+            foreach ($cfg->criteria as $cri) {
+                if ($section_id == $cri->sid) {
+                    $key = $cri->id;
+                    if(!isset($eval->evaluationData->$key)){
+                        return null;
+                    } else {
+                        $val = floatval($eval->evaluationData->$key);
+                        $totalSection += is_numeric($val) ? floatval($cri->weight) * floatval($val) : 0;
+                    }
+                }
+            }
+            $total += floatval($totalSection);
+        }
+        return $total / count($users);
+    }
 ?>
 <div class="container">
     <?php 
@@ -31,7 +60,17 @@
                     ?>
                     <th class="text-left" style="margin-top: 5px;" width="25%">Inscrição</th>
                     <th class="text-left" width="40%">Candidatos</th>
-                    <th class="text-center" width="10%"><?php echo !isset($preliminary) ? "RP" : "NF" ?></th>
+                    <?php 
+                        if(isset($preliminary)){
+                            echo '<th class="text-center" width="10%">NF</th>' ;
+                        }else{
+                            foreach($sections as $key => $sec){
+                                if(in_array($nameCat, $sec->categories)){ ?>
+                                    <th class="text-center" width="10%"><?php echo 'N'.($key + 1).'E' ?></th>
+                        <?php   }
+                            }
+                        }
+                    ?>
                 </tr>
             </thead>
             <tbody>
@@ -49,7 +88,17 @@
                             ?>
                             <td class="text-left"><?php echo $nameSub->number; ?></td>
                             <td class="text-left"><?php echo $nameSub->owner->name; ?></td>
-                            <td class="text-center"><?php echo !isset($preliminary) ? $nameSub->preliminaryResult : $nameSub->consolidatedResult; ?></td>
+                            <?php 
+                                if(isset($preliminary)){ ?>
+                                    <td class="text-center"><?php echo $nameSub->consolidatedResult; ?></td>
+                                <?php } else{
+                                    foreach($sections as $key => $sec){ 
+                                        if(in_array($nameSub->category, $sec->categories)){ ?>
+                                            <td class="text-center"><?php echo getSectionNote($opp, $nameSub, $sec->id); ?></td>
+                            <?php       } 
+                                    }
+                                }
+                            ?>
                         </tr>
                     <?php
                     }
