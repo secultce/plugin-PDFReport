b752fc1fccd0faf6225324166365c90c9c087f63
Shyko-Junior <franciscoanto@gmail.com>
2021-12-21 14:23:52 -0300

    adicionado paginas em minhas inscrições

--- Controllers/Pdf.php
+++ Controllers/Pdf.php
@@ -127,10 +127,11 @@ class Pdf extends \MapasCulturais\Controller{
         $content = $app->view->fetch($template);
 
         $footer = '<div style="border-top: 1px solid #c5c5c5;">
-        <p style="text-align: center; font-size: 10px;"><span>Escola de Saúde Pública do Ceará Paulo Marcelo Martins Rodrigues</span></p>
+        <div style="width: 100%; float: left;"><p style="text-align: center; font-size: 10px;"><span>Escola de Saúde Pública do Ceará Paulo Marcelo Martins Rodrigues</span></p>
         <p style="text-align: center; font-size: 10px;"><span>Av. Antônio Justa, 3161 - Meireles - CEP: 60.165-090</span></p>
-        <p style="text-align: center; font-size: 10px;"><span>Fortaleza / CE - Fone: (85) 3101.1398</span></p>
-        </div>';
+        <p style="text-align: center; font-size: 10px;"><span>Fortaleza / CE - Fone: (85) 3101.1398</span></p></div>
+        </div>
+        <div style="width: 100%; float: left;"><p style="text-align: center; padding-right 30px; font-size: 10px;"><span> {PAGENO}{nbpg}</span></p></div>';
                 
         $mpdf->SetHTMLFooter($footer);
         $mpdf->SetHTMLFooter($footer, 'E');
