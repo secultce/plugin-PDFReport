ed2aa6681f08c7439e4545f8283fe67419918363
Shyko-Junior <franciscoanto@gmail.com>
2021-12-21 13:11:33 -0300

    Foreach para op. sem categoria

--- views/pdf/subscribers.php
+++ views/pdf/subscribers.php
@@ -23,15 +23,19 @@
     </div>
     <div class="row" style="margin-top: 20px">
         <div class="container">
-                <?php foreach ($op->registrationCategories as $key_first => $nameCat) :?>
+                <?php 
+              
+                if($isCategory) :
+
+                foreach ($op->registrationCategories as $key_first => $nameCat) :?>
                     <div class="table-info-cat" style="margin-top: 10px">
                         <span><?php echo $nameCat; ?></span>
                     </div>
                     <table id="table-preliminar" width="100%" class="table table-striped table-bordered">
                         <thead>
                             <tr>
-                                <th class="text-left" width="20%">Inscrição</th>
-                                <th class="text-left">Candidatos</th>
+                                <th class="text-left" width="20%">INSCRIÇÃO</th>
+                                <th class="text-left">CANDIDATOS</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -58,7 +62,32 @@
                             ?>
                         </tbody>
                     </table>
-                <?php endforeach; ?>
+                <?php endforeach;
+                
+                else:
+                    ?>
+                       <table id="table-preliminar" width="100%" class="table table-striped table-bordered">
+                        <thead>
+                            <tr style="border: 1px solid #c3c3c3;" class="table-info-cat">
+                                <th class="text-left" style="padding-left: 5px; color: #fff;" width="20%">INSCRIÇÃO</th>
+                                <th class="text-left" style="color: #fff;">CANDIDATOS</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        foreach ($sub as $key => $value) { 
+                            $agent = $app->repo('Agent')->find($value->owner->id); 
+                            ?>
+                            <tr>
+                                <td class="text-left"><?php echo $value->number; ?></td>
+                                <td class="text-left"><?php echo $agent->name; ?></td>
+                                
+                            </tr>
+                                <?php } ?>
+                        </tbody>
+                    </table>
+                <?php 
+                endif; ?>
         </div>
     </div>
 </div>
