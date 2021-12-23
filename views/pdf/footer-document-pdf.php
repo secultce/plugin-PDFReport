dd9bb8e23289d08a7c1ba1a99497bcf30ef20d2e
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2021-12-23 13:01:04 -0300

    novo footer parts

---
+++ views/pdf/footer-document-pdf.php
@@ -0,0 +1,7 @@
+<div style="border-top: 1px solid #c5c5c5;">
+    <div style="width: 100%; float: left;">
+        <p style="text-align: center; font-size: 10px;"><span>Escola de Saúde Pública do Ceará Paulo Marcelo Martins Rodrigues</span></p>
+        <p style="text-align: center; font-size: 10px;"><span>Av. Antônio Justa, 3161 - Meireles - CEP: 60.165-090</span></p>
+        <p style="text-align: center; font-size: 10px;"><span>Fortaleza / CE - Fone: (85) 3101.1398</span></p>
+    </div>
+</div>
