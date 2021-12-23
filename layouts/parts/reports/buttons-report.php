4bfac6908493086166203fb5cf9b7085b6a88475
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2021-12-23 13:00:18 -0300

    altera select dinamico

--- layouts/parts/reports/buttons-report.php
+++ layouts/parts/reports/buttons-report.php
@@ -1,27 +1,15 @@
-<!--botão de imprimir-->
-<!-- <a class="btn btn-default" title="Imprimir Resultado Documental"  ng-click="editbox.open('report-evaluation-documental-options', $event)" rel="noopener noreferrer">
-    <i class="fa fa-file-text-o" aria-hidden="true"></i>
-</a>
-<a class="btn btn-default" title="Imprimir Resultado Tecnica"  ng-click="editbox.open('report-evaluation-documental-options', $event)" rel="noopener noreferrer">
-    <i class="fa fa-align-justify" aria-hidden="true"></i>
-</a>
-<a class="btn btn-default" title="Imprimir Resultado Simiples"  ng-click="editbox.open('report-evaluation-documental-options', $event)" rel="noopener noreferrer">
-    <i class="fa fa-file-o" aria-hidden="true"></i>
-</a> -->
-<?php 
+<?php
+use PDFReport\Controllers\Pdf;
 ?>
 <div>
     <hr>
     <form action="<?php echo $app->createUrl('pdf/gerarPdf'); ?>" method="GET" target="TargetWindow">
     <label class="label">Filtrar Relatório</label>
-        <select name="selectRel" id="selectRel" class="" style="margin-left: 10px;">
-            <option value="0">--Selecione--</option>
-            <option value="1">Relação de Inscritos</option>
-            <?php if($resource): ?>
-                <option value="2">Resultados preliminares</option>
-            <?php endif; ?>
-            <option value="3">Resultados definitivos</option>
-            <option value="4">Relação de contatos</option>
+        <select name="selectRel" id="selectRel" class="" style="margin-left: 10px;" required>
+            <option value="">--Selecione--</option>
+            <?php foreach(Pdf::getReportsEnabled() as $report): ?>
+                <option value="<?= $report['id'] ?>"><?= $report['title'] ?></option>
+            <?php endforeach; ?>
         </select>
         <input type="hidden" id="idopportunityReport" name="idopportunityReport">
         <button type="submit">Gerar Relatório <i class="fa fa-file-pdf-o" aria-hidden="true"></i></button>
